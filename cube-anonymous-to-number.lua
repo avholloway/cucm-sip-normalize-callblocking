ce2cbbd0c7ba4dca18f3c4aa89d5bba3bcd139c9
Anthony Holloway <avholloway@gmail.com>
2019-12-15 18:18:38 -0600

    updated description to be more clear

--- cube-anonymous-to-number.lua
+++ cube-anonymous-to-number.lua
@@ -1,7 +1,7 @@
 --[[
   If we are performing call blocking based on calling number, and we receive
   an INVITE from anonymous, it will be blocked because, CUCM will not be able
-  to use our blank nor ! XLATEs to permit calls.
+  to use our blank nor ! XLATEs we use to permit non-blacklisted callers.
 
   Therefore, we are going to find and replace these anonymous caller IDs with
   a known numeric caller ID, for the purposes of matching our ! XLATE to permit
